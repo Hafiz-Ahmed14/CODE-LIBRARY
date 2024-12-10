44b0b92382cb77368bb0def9d1acb286e57c22bb
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2024-12-10 10:56:50 +0600

    Create Read_Write_File.cpp

---
+++ Basic/File/Read_Write_File.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{    
+    
+    freopen("input_custom.txt","r",stdin);
+    freopen("output_custom.txt","w",stdout);
+
+            
+    return 0;
+}

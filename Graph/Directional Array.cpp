50e1a816ba5ec94cd4d5ac107b32c790a13c3682
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2024-12-13 18:03:24 +0600

    Create Directional Array.cpp

---
+++ Graph/Directional Array.cpp
@@ -0,0 +1,26 @@
+/***************************************************/
+/*                  Md. HAFIZ AHMED                */
+/*               DEPARTMENT OF CSE,BAUET           */
+/***************************************************/
+#include<bits/stdc++.h>
+using namespace std;
+
+//Define start and end marco
+#define BISMILLAH ios_base::sync_with_stdio(0);cin.tie(0);
+#define ALHAMDULILLAH return 0
+#define ll long long
+#define pb push_back
+#define nl '\n'
+
+// // directional array
+int dx[] = {0, -1, 0, +1, -1, -1, +1, +1};
+int dy[] = {+1, 0, -1, 0, +1, -1, -1, +1};
+
+int32_t main()
+{
+    BISMILLAH
+        
+     
+            
+    ALHAMDULILLAH;
+}

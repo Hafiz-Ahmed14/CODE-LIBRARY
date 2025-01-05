d27a160cbbd424427157e9c25b26fbc0480c976d
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2025-01-05 12:06:01 +0600

    Update Understanding_recursion.cpp

--- Basic/Recursion/Understanding_recursion.cpp
+++ Basic/Recursion/Understanding_recursion.cpp
@@ -1,1 +1,21 @@
 
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int n) {
+    cout << "We are at n = " << n << '\n';
+    if(n == 0) {
+        cout << "do not need to go further\n";
+        cout << "Return 0 from function with n " << n << '\n';
+        return 0;
+    }
+    int res = sum(n - 1) + n;
+    cout << "returning " << res << "from function with n = " << n << '\n';
+    return res;
+}
+int main()
+{    
+    cout << sum(5) << '\n'; 
+            
+    return 0;
+}

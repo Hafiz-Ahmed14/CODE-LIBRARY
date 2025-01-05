a39f21da23666b5eb5b0adc55e3ca22098053d36
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2025-01-05 13:57:35 +0600

    Create Floor_Ceil_Round.cpp

---
+++ Math/Floor_Ceil_Round.cpp
@@ -0,0 +1,12 @@
+// Ceil
+int a = 5, b = 3;
+int res = ceil((double)a / b);
+// another system 
+res = (a + b - 1) / b;
+
+
+// Floor 
+int res = floor((double)a / b);
+
+// Round
+int res = round((double)a / b);

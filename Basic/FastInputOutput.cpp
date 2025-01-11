368054987dbea78224a5285a21615ed910233f0d
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2025-01-11 14:43:26 +0600

    Add new data check for how much time need to run a program

--- Basic/FastInputOutput.cpp
+++ Basic/FastInputOutput.cpp
@@ -7,17 +7,30 @@ int main()
     ios_base::sync_with_stdio(0);cin.tie(0); 
 
     // using endl and there first go to buffer then at the time it is flush
-    for(int i = 1; i <= 10; i++) {
-        cout << i << endl;
-        this_thread::sleep_for(chrono::seconds(1));
-    }
+    // for(int i = 1; i <= 10; i++) {
+    //     cout << i << endl;
+    //     this_thread::sleep_for(chrono::seconds(1));
+    // }
 
 
     // using \n and there first go to buffer then flush it all the value at a time
-    for(int i = 1; i <= 10; i++) {
-        cout << i << '\n;
-        this_thread::sleep_for(chrono::seconds(1));
+    // for(int i = 1; i <= 10; i++) {
+    //     cout << i << '\n;
+    //     this_thread::sleep_for(chrono::seconds(1));
+    // }
+
+    // Check the program execuation time
+    clock_t st = clock();
+    int n = 1e9;
+    long long sum = 0;
+
+    for(int i = 1; i <= n; i++) {
+        sum += i;
     }
+
+    clock_t en = clock();
+
+    cout << 1.0 << (en - st) / CLOCKS_PER_SEC << "s\n;"
             
     return 0;
 }

538bdc8acf46faee4b6b157085eb8050970b0441
MD HAFIZ AHMED <147814428+Hafiz-Ahmed14@users.noreply.github.com>
2025-01-25 23:40:23 +0600

    Update BubbleSort.cpp

--- Basic/Sorting/BubbleSort.cpp
+++ Basic/Sorting/BubbleSort.cpp
@@ -5,19 +5,19 @@ using namespace std;
 int main()
 {    
     int n; cin >> n;
-    int a[n];
+    int a[n + 1];
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
         cin >> a[i];
     } 
 
-    for(int i = 1; i < n; i++) {
-        for(int j = 1; j < n; j++) {
+    for(int i = 1; i <= n; i++) {
+        for(int j = 1; j <= n; j++) {
             if(a[i] > a[j]) swap(a[i], a[j]);
         }
     }
 
-    for(int i = 0; i < n; i++) {
+    for(int i = 1; i <= n; i++) {
         cout << a[i] << ' ';
     } 
             
